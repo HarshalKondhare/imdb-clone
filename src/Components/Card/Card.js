b9aabe7136b0a62eefd8c510d467892077b0e158
HarshalKondhare <harshalkondhare9@gmail.com>
2023-10-16 19:56:20 +0530

    fourth commit

--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -1,5 +1,6 @@
 import React,{useState,useEffect} from 'react'
 import Skeleton,{SkeletonTheme} from 'react-loading-skeleton'
+import 'react-loading-skeleton/dist/skeleton.css'
 import './Card.css'
 import { Link  } from 'react-router-dom'
 
--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -50,6 +50,7 @@ const Home = () => {
             </Carousel>
 
             <MovieList/>
+            
 
            </div>
       
