04fe044903dfdafb1d2d85db53ad4041d42f691f
HarshalKondhare <harshalkondhare9@gmail.com>
2023-10-16 19:05:41 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -17,6 +17,7 @@ function App() {
           <Route  path='movies/:type' element={<MovieList/>}></Route>
           <Route  path='/' element={<h1>error page</h1>}></Route>
         </Routes>
+        <Home/>
       </Router>
 
      
